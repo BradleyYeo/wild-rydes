0db3d32769937773ab76b9c771eda9b3026ef736
Bradley Yeo <yeo.bradley@gmail.com>
2022-06-28 21:46:14 +0800

    Configure API invokeURL

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     api: {
-        invokeUrl: '' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
+        invokeUrl: 'https://8mu6vlxkr1.execute-api.ap-southeast-1.amazonaws.com/prod' // e.g. https://rc2345678345.execute-api.us-west-2.amazonaws.com/prod,
     }
 }
